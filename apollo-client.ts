10685ee3a878bbb4ec84c5fba2f86252b703d1ce
Unknown <avggonzalescsa@yahoo.com>
2022-06-07 12:57:44 +0800

    fixed env

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,10 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
-const URI =
-  process.env.NEXT_PUBLIC_VERCEL_URL &&
-  process.env.NEXT_PUBLIC_VERCEL_URL.length
-    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
-    : process.env.NEXT_PUBLIC_GRAPHQL_URL
+const URI = process.env.NEXT_PUBLIC_GRAPHQL_URL
 
 const client = new ApolloClient({
   uri: URI,
